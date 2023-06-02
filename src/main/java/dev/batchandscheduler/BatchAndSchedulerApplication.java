541e21b719d36db6f957c64f05cc0ad5bab1adee
jungu0416 <jungu0416@naver.com>
2023-06-02 10:31:06 +0900

    Batch 추가

--- src/main/java/dev/batchandscheduler/BatchAndSchedulerApplication.java
+++ src/main/java/dev/batchandscheduler/BatchAndSchedulerApplication.java
@@ -1,5 +1,6 @@
 package dev.batchandscheduler;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -7,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //DB가 필요하지 않을 시 추가
 @EnableScheduling
+@EnableBatchProcessing
 public class BatchAndSchedulerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/dev/batchandscheduler/batch/config/BatchConfig.java
@@ -0,0 +1,48 @@
+package dev.batchandscheduler.batch.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@RequiredArgsConstructor
+public class BatchConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job simpleJob() {
+        return jobBuilderFactory
+                .get("simpleJob")
+                .start(simpleStep())
+                .build();
+    }
+
+    @Bean
+    public Step simpleStep() {
+        return this.stepBuilderFactory
+                .get("simple-step")
+                .tasklet(simpleTasklet())
+                .build();
+    }
+
+    @Bean
+    public Tasklet simpleTasklet() {
+        return (stepContribution, chunkContext) -> {
+            for (int i = 10; i > 0; i--) {
+                TimeUnit.SECONDS.sleep(10);
+                System.out.println(i);
+            }
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+}
